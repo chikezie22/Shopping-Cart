3a1233fc44241804740fcc71a910df970f3948a8
chikezie22 <simonchikezie44@gmail.com>
2024-11-11 09:56:59 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route index element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import products from "../../data/product.json";
+import styles from "./ProductList.module.css";
+
 function ProductList() {
-  return <div></div>;
+  return (
+    <div className={`${styles.section}`}>
+      <h1 className={`${styles.h1}`}>Products</h1>
+      <ul className={`${styles.underline}`}>
+        {products.map((product) => {
+          return (
+            <li className={`${styles.list}`} key={product.id}>
+              <Link className={`${styles.link}`} to={`/product/${product.id}`}>
+                {/* <img src={product.image} alt={product.name} /> */}
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductList;
